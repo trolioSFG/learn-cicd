7f8fdffc4b4abb15db12f959eb2bc020359c855a
trolioSFG <sergueit@gmail.com>
2025-06-30 11:07:30 +0200

    Staticcheck test

--- main.go
+++ main.go
@@ -24,6 +24,11 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused() {
+	// Unused uncalled
+	// staticcheck test
+}
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
